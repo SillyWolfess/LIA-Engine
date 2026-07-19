2b397be575c125a59207c2c23ba674e44672e950
SillyWolfess <silier.vixen@gmail.com>
2026-07-19 09:48:19 +0200

    fix x window scale

--- code/impl/windows/settingsWindow.cpp
+++ code/impl/windows/settingsWindow.cpp
@@ -63,7 +63,7 @@ void LIA::SettingsWindow::updateData(std::string window) {
         for (AppWindow::s_windowModes& mode: resolutions) {
             resList.push_back(std::vformat("{} x {}", std::make_format_args(mode.width, mode.height)));
         }
-        UpdateGuiListEvent updateResolution2Event(_eventSource, "resolution", resList);
-        eventManager->handleEvent(updateResolution2Event);
+        UpdateGuiListEvent updateResolutionEvent(_eventSource, "resolution", resList);
+        eventManager->handleEvent(updateResolutionEvent);
     LIA_CATCH_EMPTY
 }
